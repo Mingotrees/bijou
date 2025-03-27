d59e7b57192e665cd4b8e8ebf4cfdf3b7b5bdb8e
Cris Lucero <lucerocris22@gmail.com>
2025-03-27 16:26:20 +0800

    Finished Products Admin UI (draft)

---
+++ src/js/sidebar.js
@@ -0,0 +1,25 @@
+const closeBtn2 = document.getElementById('close2');
+const categoriesBtn = document.getElementById('sidebar');
+categoriesBtn.addEventListener('click', toggleCategories);
+closeBtn2.addEventListener('click', closeCategoriesSidebar);
+
+function toggleCategories() {
+  const categoriesSidebar = document.getElementById('categories-sidebar');
+  const darkOverlay = document.getElementById('dark-overlay');
+  const isOpen = categoriesSidebar.classList.contains('-translate-x-0');
+
+  if (isOpen) {
+    closeCategoriesSidebar();
+  } else {
+    categoriesSidebar.classList.replace('-translate-x-full', 'translate-x-0');
+    darkOverlay.classList.remove('hidden');
+  }
+}
+
+function closeCategoriesSidebar() {
+  const categoriesSidebar = document.getElementById('categories-sidebar');
+  const darkOverlay = document.getElementById('dark-overlay');
+
+  categoriesSidebar.classList.replace('translate-x-0', '-translate-x-full');
+  darkOverlay.classList.add('hidden');
+}
